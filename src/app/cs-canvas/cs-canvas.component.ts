7ac00474e445cb5bcc8c9cbb81ca0e2cc5e7c242
Dako Albeik <dako.albeik@gmail.com>
2023-06-21 14:44:50 -0500

    Get cornerstone to display an image

--- src/app/cs-canvas/cs-canvas.component.ts
+++ src/app/cs-canvas/cs-canvas.component.ts
@@ -1,4 +1,12 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {cornerstone, cornerstoneTools} from '../csSetup'
 
 const IMAGE_ID = "https://rawgit.com/cornerstonejs/cornerstoneWebImageLoader/master/examples/Renal_Cell_Carcinoma.jpg";
@@ -20,68 +28,55 @@ export class CsCanvasComponent implements AfterViewInit {
   };
   imageId = this.stack.imageIds[0];
 
-  constructor() {}
+  constructor(private cdr : ChangeDetectorRef) {}
 
   ngAfterViewInit() {
-    console.log("ngAfterViewInit", this.viewport.nativeElement)
+    setTimeout(() => this.setUpCornerstone(), 0)
+  }
+
+  setUpCornerstone() {
     const element : HTMLElement = this.viewport.nativeElement;
     // Enable the DOM Element for use with Cornerstone
     cornerstone.enable(element);
-
     // Load the first image in the stack
     cornerstone.loadImage(this.imageId).then(image => {
       // Display the first image
       cornerstone.displayImage(element, image);
+      // Add the stack tool state to the enabled element
+      cornerstoneTools.addStackStateManager(element, ["stack"]);
+      cornerstoneTools.addToolState(element, "stack", this.stack);
 
-      // // Add the stack tool state to the enabled element
-      // cornerstoneTools.addStackStateManager(element, ["stack"]);
-      // cornerstoneTools.addToolState(element, "stack", this.stack);
-      //
-      // cornerstoneTools.mouseInput.enable(element);
-      // cornerstoneTools.mouseWheelInput.enable(element);
-      // cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
-      // cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
-      // cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
-      // cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
-      //
-      // cornerstoneTools.touchInput.enable(element);
-      // cornerstoneTools.panTouchDrag.activate(element);
-      // cornerstoneTools.zoomTouchPinch.activate(element);
-      //
-      // element.addEventListener(
-      //   "cornerstoneimagerendered",
-      //   this.onImageRendered
-      // );
-      // element.addEventListener("cornerstonenewimage", this.onNewImage);
-      // window.addEventListener("resize", this.onWindowResize);
+      cornerstoneTools.mouseInput.enable(element);
+      cornerstoneTools.mouseWheelInput.enable(element);
+      cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+      cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
+      cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
+      cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+
+      cornerstoneTools.touchInput.enable(element);
+      cornerstoneTools.panTouchDrag.activate(element);
+      cornerstoneTools.zoomTouchPinch.activate(element);
+
+      element.addEventListener(
+        "cornerstoneimagerendered",
+        this.onImageRendered
+      );
+      element.addEventListener("cornerstonenewimage", this.onNewImage);
+      window.addEventListener("resize", this.onWindowResize);
     });
   }
 
 
   onWindowResize() {
     console.log("onWindowResize");
-    console.log(this.viewport)
-    // cornerstone.resize(this.viewport.nativeElement);
   }
 
   onImageRendered() {
-    // const viewport = cornerstone.getViewport(this.viewport.nativeElement);
     console.log("onImageRendered")
-
-    // this.setState({
-    //   viewport
-    // });
-
-    // console.log(this.state.viewport);
   }
 
   onNewImage() {
     console.log("onNewImage")
-    // const enabledElement = cornerstone.getEnabledElement(this.viewport.nativeElement);
-
-    // this.setState({
-    //   imageId: enabledElement.image.imageId
-    // });
   }
 
 }
