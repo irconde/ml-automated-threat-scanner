01825ab34cb5efa472d9717a990d25968a88f985
James Bromley <jxbromley@ualr.edu>
2023-08-17 15:34:05 -0500

    Updating readme

--- src/app/cs-canvas/cs-canvas.component.ts
+++ src/app/cs-canvas/cs-canvas.component.ts
@@ -1,49 +1,38 @@
-import {
-  Component,
-  OnInit,
-} from '@angular/core';
-import {cornerstone, cornerstoneTools} from '../csSetup'
-import {CornerstoneDirective} from "../directives/cornerstone.directive";
-import {CornerstoneService} from "../services/cornerstone.service";
-import {getElectronAPI} from "../get-electron-api";
-import {Channels} from "../../../shared/constants/channels";
-import {FileService} from "../services/file/file.service";
-import {SettingsService} from "../services/settings/settings.service";
-import {ElectronService} from "../services/electron/electron.service";
-
+import { Component, OnInit } from '@angular/core';
+import { cornerstone } from '../csSetup';
+import { CornerstoneDirective } from '../directives/cornerstone.directive';
+import { CornerstoneService } from '../services/cornerstone.service';
+import { FileService } from '../services/file/file.service';
+import { SettingsService } from '../services/settings/settings.service';
 
 @Component({
   selector: 'app-cs-canvas',
   templateUrl: './cs-canvas.component.html',
   styleUrls: ['./cs-canvas.component.scss'],
   standalone: true,
-  imports: [
-    CornerstoneDirective
-  ],
+  imports: [CornerstoneDirective],
 })
 export class CsCanvasComponent implements OnInit {
-
-  imageData : cornerstone.Image | null = null;
+  imageData: cornerstone.Image | null = null;
   constructor(
     private csService: CornerstoneService,
     private fileService: FileService,
-    private settingsService: SettingsService) {}
+    private settingsService: SettingsService
+  ) {}
 
   ngOnInit() {
-    this.fileService.getCurrentFile().subscribe((currentFile)=> {
-      if(!currentFile.pixelData) return;
+    this.fileService.getCurrentFile().subscribe((currentFile) => {
+      if (!currentFile.pixelData) return;
+
       this.csService
         .getImageData(currentFile.fileName, currentFile.pixelData)
-        .subscribe((image)=> {
-            this.imageData = image;
-          }
-        )
-    })
+        .subscribe((image) => {
+          this.imageData = image;
+        });
+    });
   }
-
 
   handleChangeImage(next = true) {
     this.fileService.requestNextFile(next);
   }
-
 }
