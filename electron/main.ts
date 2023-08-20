4d280e0b01606c44290832d85b19cce6e2f18428
James <jspbromley@gmail.com>
2023-08-20 16:11:37 -0500

    Renaming of services and components. Removal of project template tabs

--- electron/main.ts
+++ electron/main.ts
@@ -1,51 +1,52 @@
-import {CachedSettings} from "./file-managers/cached-settings";
-import UserFilesManager from "./file-managers/user-files-manager";
-import { app, BrowserWindow } from 'electron'
+import { CachedSettings } from './file-managers/cached-settings';
+import UserFilesManager from './file-managers/user-files-manager';
+import { app, BrowserWindow } from 'electron';
 
-const path = require("path");
+const path = require('path');
 const isDev = require('electron-is-dev');
-let userFilesManager : null | UserFilesManager = null;
+let userFilesManager: null | UserFilesManager = null;
 
-
-const createWindow = async () : Promise<BrowserWindow> => {
+const createWindow = async (): Promise<BrowserWindow> => {
   const mainWindow = new BrowserWindow({
     width: 1600,
     height: 1200,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-  const htmlPath = isDev ?
-        "http://localhost:4200" :
-        `file://${path.join(__dirname, "..", "www", "index.html")}`;
+  const htmlPath = isDev
+    ? 'http://localhost:4200'
+    : `file://${path.join(__dirname, '..', 'www', 'index.html')}`;
 
   try {
-    await mainWindow.loadURL(htmlPath)
+    await mainWindow.loadURL(htmlPath);
     return mainWindow;
   } catch (e) {
     throw e;
   }
-}
+};
 
 app.whenReady().then(async () => {
-
-  const mainWindow = await createWindow()
+  const mainWindow = await createWindow();
   const settings = await CachedSettings.create(mainWindow);
-  console.log("SETTINGS", settings.get())
-  await settings.update({selectedAnnotationFile: "", selectedImagesDirPath: "G:\\EAC\\images\\coco\\val2017-small-test"})
-  console.log(settings.get())
-  userFilesManager = new UserFilesManager(settings, mainWindow)
+  console.log('SETTINGS', settings.get());
+  await settings.update({
+    selectedAnnotationFile: '',
+    selectedImagesDirPath: 'G:\\EAC\\images\\coco\\val2017-small-test',
+  });
+  console.log(settings.get());
+  userFilesManager = new UserFilesManager(settings, mainWindow);
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
--- src/app/services/file-parser/file-parser.service.ts
+++ src/app/services/file-parser/file-parser.service.ts
@@ -1,14 +1,19 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import JSZip from 'jszip';
-import {AnnotationType, ParsedORA, PixelData} from "../../models/file-parser";
-import {v4 as guid} from 'uuid'
+import {
+  AnnotationType,
+  ParsedORA,
+  PixelData,
+} from '../../../models/file-parser';
+import { v4 as guid } from 'uuid';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FileParserService {
   private zipUtil: JSZip;
   private domParser: DOMParser;
+
   constructor() {
     this.zipUtil = new JSZip();
     this.domParser = new DOMParser();
@@ -21,59 +26,63 @@ export class FileParserService {
       return await this.loadFilesData(parsedOra);
     } catch (e) {
       console.log(e);
-      throw Error("Failed to load data")
+      throw Error('Failed to load data');
     }
   }
 
-  private async toXmlDoc(fileData: string) : Promise<Document> {
-    await this.zipUtil.loadAsync(fileData, {base64: false})
+  private async toXmlDoc(fileData: string): Promise<Document> {
+    await this.zipUtil.loadAsync(fileData, { base64: false });
     const stackFile = this.zipUtil.file('stack.xml');
-    if(!stackFile) throw Error("Failed to find stack.xml");
+    if (!stackFile) throw Error('Failed to find stack.xml');
     const stackString = await stackFile.async('string');
-    return this.domParser.parseFromString(stackString, "text/xml");
+    return this.domParser.parseFromString(stackString, 'text/xml');
   }
 
-  private async parseXmlDoc(xmlDoc: Document) : Promise<ParsedORA> {
+  private async parseXmlDoc(xmlDoc: Document): Promise<ParsedORA> {
     const xmlImages = xmlDoc.getElementsByTagName('image');
-    if(!xmlImages[0]) throw Error("Failed to find image element")
+    if (!xmlImages[0]) throw Error('Failed to find image element');
     const currentFileFormat = xmlImages[0].getAttribute('format');
-    const parsedORA : ParsedORA = {
-      format: currentFileFormat as AnnotationType || AnnotationType.UNKNOWN,
+    const parsedORA: ParsedORA = {
+      format: (currentFileFormat as AnnotationType) || AnnotationType.UNKNOWN,
       viewpoints: [],
     };
-    const stacks = Array.from(xmlImages[0].children)
+    const stacks = Array.from(xmlImages[0].children);
     stacks.forEach((stack) => {
-
-      const layers : Element[] = Array.from(stack.children);
+      const layers: Element[] = Array.from(stack.children);
       const firstLayer = layers.shift();
-      if(!firstLayer) throw Error("No layers exist in stack")
+      if (!firstLayer) throw Error('No layers exist in stack');
 
       const pixelData = firstLayer.getAttribute('src');
-      if(!pixelData) throw Error("Missing src attribute on first layer")
+      if (!pixelData) throw Error('Missing src attribute on first layer');
 
       if (parsedORA.format === AnnotationType.UNKNOWN) {
         parsedORA.format = this.getAnnotationType(pixelData);
       }
 
       const viewpoint = stack.getAttribute('view');
-      if(!viewpoint) throw Error("Missing view attribute on stack");
+      if (!viewpoint) throw Error('Missing view attribute on stack');
 
       const imageId = stack.getAttribute('name')?.replace(/^\D+/g, '');
-      if(!imageId) throw Error("Missing image id on stack");
+      if (!imageId) throw Error('Missing image id on stack');
 
       const detectionData: string[] = [];
-      layers.forEach((layer)=> {
+      layers.forEach((layer) => {
         const src = layer.getAttribute('src');
         src && detectionData.push(src);
-      })
+      });
 
-      parsedORA.viewpoints.push({ viewpoint, pixelData, imageId, detectionData });
+      parsedORA.viewpoints.push({
+        viewpoint,
+        pixelData,
+        imageId,
+        detectionData,
+      });
     });
 
     return parsedORA;
   }
 
-  private getAnnotationType(imageSrc: string) : AnnotationType {
+  private getAnnotationType(imageSrc: string): AnnotationType {
     const fileExtension = imageSrc.split('.').pop();
     switch (fileExtension) {
       case 'dcs':
@@ -83,40 +92,43 @@ export class FileParserService {
       case 'jpeg':
         return AnnotationType.COCO;
       default:
-        throw Error("Failed to handle file type")
+        throw Error('Failed to handle file type');
     }
   }
 
-  private async loadFilesData(parsedOR: ParsedORA): Promise<{detectionData: any[], imageData: PixelData[]}> {
-    const { format } = parsedOR
+  private async loadFilesData(
+    parsedOR: ParsedORA
+  ): Promise<{ detectionData: any[]; imageData: PixelData[] }> {
+    const { format } = parsedOR;
     const detectionData: any[] = [];
     const allPromises: Promise<string | Uint8Array | Blob | ArrayBuffer>[] = [];
-    const imageData : PixelData[] = [];
+    const imageData: PixelData[] = [];
     parsedOR.viewpoints.forEach((canvasViewpoint) => {
       // load detection data
       canvasViewpoint.detectionData.forEach((detectionPath) => {
         const detectionFile = this.zipUtil.file(detectionPath);
-        if(!detectionFile) throw Error("Failed to load detection data")
+        if (!detectionFile) throw Error('Failed to load detection data');
 
-        const fileType = format === AnnotationType.COCO ? 'string' : 'uint8array';
+        const fileType =
+          format === AnnotationType.COCO ? 'string' : 'uint8array';
         const promise = detectionFile.async(fileType);
         allPromises.push(promise);
 
         promise.then((data) => {
-           switch (format) {
-             // TODO: read detection data here and push onto detection array
-             case AnnotationType.COCO:
-               break;
-             case AnnotationType.TDR:
-               break;
-             default:
-               throw Error("Annotation type not supported")
-           }
+          switch (format) {
+            // TODO: read detection data here and push onto detection array
+            case AnnotationType.COCO:
+              break;
+            case AnnotationType.TDR:
+              break;
+            default:
+              throw Error('Annotation type not supported');
+          }
         });
       });
       // load pixel data
       const pixelFile = this.zipUtil.file(canvasViewpoint.pixelData);
-      if(!pixelFile) throw Error("Failed to load pixel data")
+      if (!pixelFile) throw Error('Failed to load pixel data');
       const fileType = format === AnnotationType.COCO ? 'arraybuffer' : 'blob';
       const promise = pixelFile.async(fileType);
       allPromises.push(promise);
@@ -125,7 +137,8 @@ export class FileParserService {
         imageData.push({
           viewpoint: canvasViewpoint.viewpoint,
           pixelData: data,
-          imageId: format === AnnotationType.COCO ? canvasViewpoint.imageId : guid(),
+          imageId:
+            format === AnnotationType.COCO ? canvasViewpoint.imageId : guid(),
           type: format,
         });
       });
@@ -134,5 +147,4 @@ export class FileParserService {
     await Promise.all(allPromises);
     return { detectionData, imageData };
   }
-
 }
