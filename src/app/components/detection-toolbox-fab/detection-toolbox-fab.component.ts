3fc62f7278d9edadd30e8e2af432253f08750b71
Dako Albeik <mfalbeik@ualr.edu>
2023-09-15 15:29:36 -0500

    Put final touches on styles

--- src/app/components/detection-toolbox-fab/detection-toolbox-fab.component.ts
+++ src/app/components/detection-toolbox-fab/detection-toolbox-fab.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-detection-toolbox-fab',
   templateUrl: './detection-toolbox-fab.component.html',
   styleUrls: ['./detection-toolbox-fab.component.scss'],
   standalone: true,
+  imports: [MatIconModule],
 })
 export class DetectionToolboxFabComponent implements OnInit {
   constructor() {}
