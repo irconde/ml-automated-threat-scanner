3d5e17115cc1bc02ff5886542796b40e39e95481
Luka Woodson <llwoodson@ualr.edu>
2023-09-19 01:55:51 -0500

    Add all svgs to files and create outline of topbar html

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { FileService } from '../services/file/file.service';
+import { SettingsService } from '../services/settings/settings.service';
+import { SettingsModalComponent } from '../settings-modal/settings-modal.component';
+import { ApplicationSettings } from '../services/settings/models/Settings';
+import { FilePayload } from '../../../shared/models/file-models';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { WorkingMode } from '../../enums/platforms';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss'],
+  standalone: true,
+  imports: [
+    SettingsModalComponent,
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+})
+export class TopBarComponent implements OnInit {
+  currentFile: FilePayload | null = null;
+  settings: ApplicationSettings | null = null;
+  connectionStatus: string = '';
+
+  constructor(
+    public fileService: FileService,
+    public settingsService: SettingsService,
+    public dialog: MatDialog,
+  ) {}
+
+  ngOnInit() {
+    this.fileService.getCurrentFile().subscribe((currentFile) => {
+      this.currentFile = currentFile;
+    });
+
+    this.settingsService
+      .getSettings()
+      .subscribe((settings: ApplicationSettings | null) => {
+        this.settings = settings;
+        // settings are null when they are first loading
+        if (settings && SettingsService.isMissingRequiredInfo(settings)) {
+          this.openSettingsModal();
+        }
+      });
+    this.updateConnectionStatus();
+  }
+
+  openSettingsModal() {
+    this.dialog.open(SettingsModalComponent, {
+      autoFocus: false,
+    });
+  }
+
+  // TODO: make file queue icon update number based on the currentFile.filesCount
+
+  updateConnectionStatus() {
+    // REFACTOR: this is not showing the proper status
+    this.connectionStatus =
+      this.settings?.workingMode === WorkingMode.RemoteServer
+        ? 'cloud'
+        : 'cloud_off';
+  }
+
+  // updateTraffic(){
+  //   // TODO: update traffic icon based on current traffic
+  //   switch () {
+  //       case 'downloadAndUpload':
+  //           return 'assets/traffic icons/traffic-download-upload.icon.svg';
+  //       case 'noTransmission':
+  //           return 'assets/traffic icons/traffic-no-transmission.icon.svg';
+  //       case 'downloading':
+  //           return 'assets/traffic icons/traffic-download.icon.svg';
+  //       case 'uploading':
+  //           return 'assets/traffic icons/traffic-upload.icon.svg';
+  //       default:
+  //           return null;
+  //   }
+  // }
+
+  toggleSideMenu() {}
+}
