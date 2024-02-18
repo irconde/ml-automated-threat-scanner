7cfa80c5bae346cbcf2632cf5a5c545bd5e3f81b
James <jxbromley@ualr.edu>
2024-02-18 16:36:18 -0600

    Next and Save button styling

--- src/app/components/side-menu/next-button/next-button.component.ts
+++ src/app/components/side-menu/next-button/next-button.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-next-button',
   templateUrl: './next-button.component.html',
-  styleUrls: ['./next-button.component.scss'],
+  styleUrls: [
+    '../shared-button.component.scss',
+    './next-button.component.scss',
+  ],
   standalone: true,
+  imports: [MatButtonModule],
 })
 export class NextButtonComponent {
   constructor() {}
--- src/app/components/side-menu/save-button/save-button.component.ts
+++ src/app/components/side-menu/save-button/save-button.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-save-button',
   templateUrl: './save-button.component.html',
-  styleUrls: ['./save-button.component.scss'],
+  styleUrls: [
+    '../shared-button.component.scss',
+    './save-button.component.scss',
+  ],
   standalone: true,
+  imports: [MatButtonModule, IonicModule],
 })
 export class SaveButtonComponent {
   constructor() {}
