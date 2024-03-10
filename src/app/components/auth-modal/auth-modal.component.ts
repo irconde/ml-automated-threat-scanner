2f67a9c040617f022dc08ceff5f755588d29c424
Dako Albeik <mfalbeik@ualr.edu>
2024-03-10 17:06:22 -0500

    Refactor register form to use new input component

--- src/app/components/auth-modal/auth-modal.component.ts
+++ src/app/components/auth-modal/auth-modal.component.ts
@@ -7,7 +7,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AuthService } from '../../services/auth/auth.service';
-import { CustomInputComponent } from '../custom-input/custom-input.component';
 import { NgIf, NgOptimizedImage } from '@angular/common';
 import { UnderlineInputComponent } from '../underline-input/underline-input.component';
 import {
@@ -16,11 +15,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import {
-  ControlErrorCode,
-  CustomValidators,
-} from '../underline-input/custom-validators';
-import { assertUnreachable } from '../../utilities/general.utilities';
+import { CustomValidators } from '../underline-input/custom-validators';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -37,7 +32,6 @@ import { MatDialogRef } from '@angular/material/dialog';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    CustomInputComponent,
     NgOptimizedImage,
     NgIf,
     UnderlineInputComponent,
@@ -57,55 +51,28 @@ export class AuthModalComponent {
       validators: [Validators.required, CustomValidators.password()],
     }),
   });
-
-  inputInfo = {
-    vaildForms: {
-      login: true,
-      signup: true,
-    },
-    username: {
-      leadingIcon: 'person',
-      placeholder: 'Username*',
-      type: 'text',
-    },
-    password: {
-      leadingIcon: 'lock',
-      placeholder: 'Password*',
-      inlineIcon: 'visibility',
-      altInlineIcon: 'visibility_off',
-      type: 'password',
-    },
-    email: {
-      leadingIcon: 'email',
-      placeholder: 'Email address*',
-      type: 'email',
-    },
-  };
+  registerForm = new FormGroup({
+    username: new FormControl('', {
+      validators: [Validators.required, CustomValidators.username()],
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, CustomValidators.email()],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, CustomValidators.password()],
+    }),
+  });
 
   constructor(
     private dialogRef: MatDialogRef<AuthModalComponent>,
     public authService: AuthService,
   ) {}
 
-  handleInputValidation(inputValue: string, inputType: string) {
-    switch (inputType) {
-      case 'text':
-        this.isFormValid = /^[a-zA-Z]+$/.test(inputValue.trim());
-        break;
-      case 'password':
-        this.isFormValid = /^[A-Za-z0-9!@#$^&()_-]+$/.test(inputValue.trim());
-        break;
-      case 'email':
-        this.isFormValid = /@\w+\.\w+/.test(inputValue.trim());
-        break;
-    }
-  }
-
   determineErrors(form: FormGroup) {
     let formInvalid = false;
     for (const controlName in form.controls) {
       const formControl = form.controls[controlName] as FormControl;
-      const errorMsg = this.getInputError(formControl);
+      const errorMsg = CustomValidators.getInputError(formControl);
       form.controls[controlName].setErrors({ errorMsg });
       if (errorMsg) {
         formInvalid = true;
@@ -114,32 +81,8 @@ export class AuthModalComponent {
     return formInvalid;
   }
 
-  getInputError(formControl: FormControl): string {
-    const errorCode = formControl.errors
-      ? (Object.keys(formControl.errors)[0] as ControlErrorCode)
-      : null;
-    switch (errorCode) {
-      case null:
-        return '';
-      case ControlErrorCode.ErrorMsg:
-        return formControl.errors?.['errorMsg'];
-      case ControlErrorCode.Required:
-        return 'Field is required';
-      case ControlErrorCode.Email:
-        return 'Email is invalid';
-      case ControlErrorCode.Username:
-        return 'Can only use letters';
-      case ControlErrorCode.Password:
-        return 'Not valid. A-Z and a-z, 0-9, and ! @ # $ ^ & ( ) _ - only';
-    }
-
-    // ensures all error codes are handled
-    return assertUnreachable(errorCode);
-  }
-
   async handleLogin(event: SubmitEvent) {
     event.preventDefault();
-    console.log(this.loginForm.value);
     if (this.determineErrors(this.loginForm)) {
       return;
     }
@@ -179,8 +122,6 @@ export class AuthModalComponent {
       }
     }
   }
-
-  protected readonly Boolean = Boolean;
 
   loginDisabled() {
     return !(
--- src/app/components/underline-input/custom-validators.ts
+++ src/app/components/underline-input/custom-validators.ts
@@ -1,4 +1,10 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+import { assertUnreachable } from '../../utilities/general.utilities';
 
 export enum ControlErrorCode {
   Required = 'required',
@@ -32,5 +38,40 @@ export class CustomValidators {
         ? null
         : { [ControlErrorCode.Password]: true };
     };
+  }
+
+  static email(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+
+      if (!value) return null;
+
+      return /@\w+\.\w+/.test(value)
+        ? null
+        : { [ControlErrorCode.Email]: true };
+    };
+  }
+
+  static getInputError(formControl: FormControl): string {
+    const errorCode = formControl.errors
+      ? (Object.keys(formControl.errors)[0] as ControlErrorCode)
+      : null;
+    switch (errorCode) {
+      case null:
+        return '';
+      case ControlErrorCode.ErrorMsg:
+        return formControl.errors?.['errorMsg'];
+      case ControlErrorCode.Required:
+        return 'Field is required';
+      case ControlErrorCode.Email:
+        return 'Email is invalid';
+      case ControlErrorCode.Username:
+        return 'Can only use letters';
+      case ControlErrorCode.Password:
+        return 'Not valid. A-Z and a-z, 0-9, and ! @ # $ ^ & ( ) _ - only';
+    }
+
+    // ensures all error codes are handled
+    return assertUnreachable(errorCode);
   }
 }
